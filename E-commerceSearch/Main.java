d417c69e799167c718e798a29078f41faae469b2
Tiyasha0811 <tiyasha08112003@gmail.com>
2026-06-24 11:25:40 +0530

    Add files via upload

---
+++ E-commerceSearch/Main.java
@@ -0,0 +1,93 @@
+class Product {
+
+    int productId;
+    String productName;
+    String category;
+
+    Product(int productId, String productName, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+
+    void display() {
+        System.out.println(productId + " " + productName + " " + category);
+    }
+}
+
+public class Main {
+
+    // Linear Search
+    public static Product linearSearch(Product[] products, int key) {
+
+        for (Product p : products) {
+            if (p.productId == key) {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    // Binary Search
+    public static Product binarySearch(Product[] products, int key) {
+
+        int low = 0;
+        int high = products.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (products[mid].productId == key) {
+                return products[mid];
+            }
+
+            else if (products[mid].productId < key) {
+                low = mid + 1;
+            }
+
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        // Sorted by Product ID
+        Product[] products = {
+
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Phone", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Bag", "Fashion")
+
+        };
+
+        int searchId = 104;
+
+        System.out.println("Linear Search:");
+
+        Product result1 = linearSearch(products, searchId);
+
+        if (result1 != null)
+            result1.display();
+        else
+            System.out.println("Product Not Found");
+
+        System.out.println();
+
+        System.out.println("Binary Search:");
+
+        Product result2 = binarySearch(products, searchId);
+
+        if (result2 != null)
+            result2.display();
+        else
+            System.out.println("Product Not Found");
+    }
+}
